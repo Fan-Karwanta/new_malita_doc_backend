89839da23418876bc1f75df220ef907f776fdb43
Fan Karwanta <fankarwanta@Fans-MacBook-Air.local>
2025-03-27 10:00:34 +0800

    fixing profile page

--- controllers/userController.js
+++ controllers/userController.js
@@ -200,14 +200,14 @@ const updateProfile = async (req, res) => {
 
     try {
 
-        const { userId, name, phone, address, dob, gender } = req.body
+        const { userId, firstName, lastName, phone, address, dob, gender } = req.body
         const imageFile = req.file
 
-        if (!name || !phone || !dob || !gender) {
+        if (!firstName || !lastName || !phone || !dob || !gender) {
             return res.json({ success: false, message: "Data Missing" })
         }
 
-        await userModel.findByIdAndUpdate(userId, { name, phone, address: JSON.parse(address), dob, gender })
+        await userModel.findByIdAndUpdate(userId, { firstName, lastName, phone, address: JSON.parse(address), dob, gender })
 
         if (imageFile) {
 
